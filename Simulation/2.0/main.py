f2803eb93fd28370092cb2547379bcf2f7cdb8ae
Martin Bataille <martinbataille@gmail.com>
2016-08-31 22:14:31 -0300

    Working on moment of inertia

--- Simulation/2.0/main.py
+++ Simulation/2.0/main.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 #from scipy.optimize import curve_fit
 import scipy.fftpack
 
-NI = 500
+NI = 5000
 N = 2048
 dt = 20. / N
 
@@ -32,7 +32,7 @@ def solution(I, ang = np.pi/2., t = np.linspace(0,20,N)):
 def solEstim():
     g = 9.8
     la = .77
-    I = np.linspace(0,10, NI)
+    I = np.linspace(0,1, NI)
     m = 0.65
     k = np.sqrt((g*la)/(I/m + la*la))
     T = 2*np.pi/k
@@ -53,18 +53,20 @@ def estim(t,alpha):
     plt.show()
 
 def inertia():
-    I = np.linspace(0.1, 10, NI)
-    ang = [np.pi/2., np.pi/4., np.pi/8.]
+    I = np.linspace(0, 1, NI)
+    ang = [30*np.pi/180., 20*np.pi/180.]
     for c in ang:    
         T = []
         for i in I:
-            t = np.linspace(0,20,N)
+            t = np.linspace(0,5,N)
             sol = diff.inertiaSol(i ,conf_ini = [c, 0.],time = t)
             alpha = sol[:,0]
             T.append(period(t,alpha))
-        plt.plot(I, T, label = 'pi/{}'.format(int(np.pi/c)))
+        plt.plot(I, T, label = '{}°'.format(int(round(180.*c/np.pi))))
     x,y = solEstim()
     plt.plot(x,y, label = 'analytique')
+    plt.plot([0,1],[1.91,1.91], 'r-', label = 'T')
+    plt.xticks(np.arange(I[0], I[-1], 0.1))
 
 def calcInertia(d, l1, l2, alpha):
     tan = np.tan(alpha)
@@ -74,12 +76,24 @@ def calcInertia(d, l1, l2, alpha):
                     + (l1*(1+tan*tan)*(l1*l1*l1-l2*l2*l2*cos*cos*cos)/3.) 
                     - (tan*tan * (l1*l1*l1*l1-l2*l2*l2*l2*cos*cos*cos*cos))/12.)
     return a + b
-    
+
+def recInertia(l):
+    d = 540
+    a = 0.0254
+    b = 2 * a
+    s = 0.
+    for c in l:
+        m = a * b * c * d    
+        s += m*(2*a*a + 2*b*b + 2*c*c)/12.
+    return s
 #t = np.linspace(0, 20, N)
 #estim(t, solution(3))
-inertia()
-plt.legend()
-plt.show()
+    
+#inertia()
+#plt.legend()
+#plt.show()
+l = [0.63, 0.41 ]
+print(recInertia(l))
 #s = 0
 #n = int(input('n '))
 #for i in range(n):
